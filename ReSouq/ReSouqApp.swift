cacff3de406a329d920bec9bf55dc8997b03c3ae
mahasultan <ajabor@andrew.cmu.edu>
2025-02-13 14:31:56 +0300

    Initial Commit

---
+++ ReSouq/ReSouqApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReSouqApp.swift
+//  ReSouq
+//
+//  Created by Al Maha Al Jabor on 13/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct ReSouqApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ReSouqTests/ReSouqTests.swift
@@ -0,0 +1,17 @@
+//
+//  ReSouqTests.swift
+//  ReSouqTests
+//
+//  Created by Al Maha Al Jabor on 13/02/2025.
+//
+
+import Testing
+@testable import ReSouq
+
+struct ReSouqTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
