be4eb6051a462f9670b566b9b116c98f0506e302
hmkhalif <hmkhalif@andrew.cmu.edu>
2025-03-09 13:41:58 +0300

    fixing homeview to call products

--- ReSouq/Views/HomeView.swift
+++ ReSouq/Views/HomeView.swift
@@ -7,6 +7,7 @@ import SwiftUI
 
 struct HomeView: View {
     @EnvironmentObject var authViewModel: AuthViewModel
+    @StateObject var productVM = ProductViewModel()
 
     var body: some View {
         NavigationStack {
@@ -14,6 +15,28 @@ struct HomeView: View {
                 VStack {
                     TopBarView(showLogoutButton: true, showAddButton: true)
                         .environmentObject(authViewModel)
+
+                    VStack(alignment: .leading) {
+                        Text("New Listings")
+                            .font(.custom("ReemKufi-Bold", size: 22))
+                            .padding(.horizontal)
+
+                        ScrollView(.horizontal, showsIndicators: false) {
+                            HStack(spacing: 15) {
+                                if productVM.products.isEmpty {
+                                    Text("No products found.")
+                                        .foregroundColor(.red)
+                                        .padding()
+                                } else {
+                                    ForEach(productVM.sortedProducts) { product in
+                                        ProductItem(product: product)
+                                    }
+                                }
+                            }
+                            .padding(.horizontal)
+                        }
+                    }
+                    .padding(.top, 10)
 
                     Spacer()
                 }
@@ -24,6 +47,19 @@ struct HomeView: View {
                 }
                 .ignoresSafeArea(.all, edges: .bottom)
             }
+            .onAppear {
+                productVM.fetchProducts()
+            }
         }
+    }
+}
+
+
+// Preview
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+            .environmentObject(AuthViewModel())
+            .environmentObject(ProductViewModel())
     }
 }
