785a4c48f149bad95e0836a228027fa4b7520976
hmkhalif <hmkhalif@andrew.cmu.edu>
2025-03-10 05:03:25 +0300

    adding view to list all categories

---
+++ ReSouq/Views/CategoryViews/CategoriesView.swift
@@ -0,0 +1,24 @@
+//
+//  CategoriesView.swift
+//  ReSouq
+//
+//
+
+
+import SwiftUI
+
+struct CategoriesView: View {
+    @StateObject var categoryVM = CategoryViewModel()
+
+    var body: some View {
+        List(categoryVM.categories) { category in
+            NavigationLink(destination: CategoryProductsView(categoryID: category.id, categoryName: category.name)) {
+                Text(category.name)
+            }
+        }
+        .navigationTitle("All Categories")
+        .onAppear {
+            categoryVM.fetchCategories()
+        }
+    }
+}
