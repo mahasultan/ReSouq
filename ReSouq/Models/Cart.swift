abcd0d923bc98f92b47aea068be0f2bc8ebe8138
shahad1307 <sastaneh@andrew.cmu.edu>
2025-03-17 14:32:36 +0300

    fixed some cart issues

--- ReSouq/Models/Cart.swift
+++ ReSouq/Models/Cart.swift
@@ -11,19 +11,13 @@ struct Cart: Identifiable, Codable {
     var products: [CartItem] = []
     
     var totalPrice: Double {
-        return products.reduce(0) { $0 + ($1.product.price * Double($1.quantity)) }
+        return products.reduce(0) { $0 + $1.product.price }
     }
 }
 
 struct CartItem: Identifiable, Codable {
     var id: String
     var product: Product
-    var quantity: Int
 
-    init(product: Product, quantity: Int) {
-        self.id = "\(product.id ?? UUID().uuidString)_\(UUID().uuidString)"
-        self.product = product
-        self.quantity = quantity
-    }
 }
 
